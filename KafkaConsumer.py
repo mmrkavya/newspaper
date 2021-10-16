71016a03112b025e96be8fbe056ab15c2fc67fdb
kavya <mmrkavya@gmail.com>
2021-10-16 11:28:58 +0530

    added category

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -2,13 +2,41 @@
 import json
 from kafka import KafkaConsumer
 import pymongo
+from newspaper import Article
+import nltk
 
+#only first time
+nltk.download('punkt')
+ 
 client = pymongo.MongoClient("mongodb://kavya:BzSz97SmGzU9ZKL6@cluster0-shard-00-00.rhrte.mongodb.net:27017,cluster0-shard-00-01.rhrte.mongodb.net:27017,cluster0-shard-00-02.rhrte.mongodb.net:27017/newspaper?ssl=true&replicaSet=atlas-q05w9f-shard-0&authSource=admin&retryWrites=true&w=majority")
 newspaperdb=client['newspaper']
 collection=newspaperdb['newspaperFeed']
 
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092',auto_offset_reset='earliest', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 consumer.subscribe(['newspaper'])
+#dict_keys(['source', 'author', 'title', 'description', 'url', 'urlToImage', 'publishedAt', 'content','category'])
 for message in consumer :
-    collection.insert_one(message.value)
-    print(message.value)
+	try:
+		a={}
+		insertValue=message.value
+		a['source']=insertValue['source']['name']
+		a['author']=insertValue['author']
+		a['title']=insertValue['title']
+		a['publishedAt']=insertValue['publishedAt']
+		a['category']=insertValue['category']
+		print(f'----------cateogry {a["category"]}')
+		url=insertValue['url']
+		#print(url)
+		article=Article(url)
+		article.download()
+		article.parse()
+		article.nlp()
+		#print(article.summary)
+		a['summary']=article.summary
+		a['article']=article.text
+		collection.insert_one(insertValue)
+		print(insertValue)
+	except Exception as e:
+		print(f'error while parsing, not inserted into db --> {e}')
+		
+ 
--- KafkaProducer.py
+++ KafkaProducer.py
@@ -2,11 +2,11 @@
 from kafka import KafkaProducer
 import numpy as np
 import pandas as pd
-from json
+from json import dumps
 import time
-from bson import json_util
+import random
 import datetime
-import requests
+import requests 
 
 #Declaring Kafka_Broker Port
 KAFKA_BROKER = 'localhost:9092'
@@ -14,23 +14,46 @@ KAFKA_BROKER = 'localhost:9092'
 KAFKA_TOPIC = 'newspaper'
 
 try:
-	producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER)
+	producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER,value_serializer=lambda x: 
+									 dumps(x).encode('utf-8'))
 except Exception as e:
 	print(f'Error Connecting to Kafka --> {e}')
 
 
+#sending  one record at a time
+#class NpEncoder(json.JSONEncoder):
+ #	 def default(self, obj):
+  #		 if isinstance(obj, np.integer):
+   #		 return int(obj)
+	#	 if isinstance(obj, np.floating):
+	 #		 return float(obj)
+	  #	 if isinstance(obj, np.ndarray):
+	   #	 return obj.tolist()
+		#return super(NpEncoder, self).default(obj)
+
 #Reading each Record one by one so we are writing it in a loop
-end=datetime.datetime.today()
+end1=datetime.datetime.today()
+end=end1
 start=end-datetime.timedelta(minutes=14)
+categories=['business','entertainment','general','health','science','sports','technology']
+
 while True:
-	
-	response=requests.get("https://newsapi.org/v2/top-headlines?from={startdatetime}&to=(enddatetime)&sortBy=publishedAt&apiKey=fe0e70f78de749aeab74f31c65636ba0&country=in".format(startdatetime=start.strftime("%Y-%m-%dT%H:%M:%S"),enddatetime=end.strftime("%Y-%m-%dT%H:%M:%S")))
-	articles=dict(response.json())['articles']
-	start=datetime.datetime.today()
-	end=start+datetime.timedelta(minutes=14)
-	for article in articles:
-		print(article)
-		print('--------------')
-		producer.send(KAFKA_TOPIC, json.dumps(data, default=json_util.default).encode('utf-8'))
-		time.sleep(2)
-	time.sleep(900)
+		count=0
+		
+		for c in categories:
+			response=requests.get("https://newsapi.org/v2/top-headlines?from={startdatetime}&to=(enddatetime)&sortBy=publishedAt&apiKey=fe0e70f78de749aeab74f31c65636ba0&country=in&category={category}".format(startdatetime=start.strftime("%Y-%m-%dT%H:%M:%S"),enddatetime=end.strftime("%Y-%m-%dT%H:%M:%S"),category=c))
+			articles=dict(response.json())['articles']
+			#start=datetime.datetime.today()
+			#end=start+datetime.timedelta(minutes=14)
+			if count == 0:
+				end1=datetime.datetime.today()
+			count=count+1
+			for article in articles:
+				print(article)
+				article['category']=c
+				print('--------------')
+				producer.send(KAFKA_TOPIC, value=article)
+				time.sleep(2)
+		start=end+datetime.timedelta(seconds=1)
+		end=end1
+		time.sleep(900)
